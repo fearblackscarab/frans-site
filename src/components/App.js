bc85e93c7de657e6a0a2615aa6e05447d4cb85e7
FearBlackScarab <fearblackscarab@gmail.com>
2022-04-30 22:02:51 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
